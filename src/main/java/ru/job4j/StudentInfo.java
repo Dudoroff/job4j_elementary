ffd13c21e035dcef0576dd9e61dd823d41427a26
Dudoroff <ilya.dudorov.dev@gmail.com>
2021-09-06 23:03:27 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ilya Dudorov");
+        System.out.println("08.08.1994");
+
+    }
+}
